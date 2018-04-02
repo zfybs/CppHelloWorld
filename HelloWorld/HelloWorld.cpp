33d5e95705de8417c8ce6e2ff9016ec73783626a
Administrator <Administrator@ZENGFY>
2018-04-02 13:22:07 +0800

    Add project files.

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,18 @@
+// HelloWorld.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "iostream"
+using namespace std;
+
+int main()
+{
+	std::cout << "在 Console 窗口中显示的内容" << std::endl;
+	int a;
+	std::cout << "输入变量a的值 \n";
+	std::cin >> a;
+	std::cout << "a的值为" << a;
+
+	return 0;
+}
+
